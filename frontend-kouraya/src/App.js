0267695e5e5a35add3fca61d9c2ed70ce4afbca4
Memphis Malik <thisismemphismalik@gmail.com>
2022-04-14 14:42:08 +0000

    Firebase Set Up Commit

--- frontend-kouraya/src/App.js
+++ frontend-kouraya/src/App.js
@@ -1,24 +1,8 @@
 import './App.css';
-// import { firebase_app } from './firebase' hello mes people;
+import {database} from "./firebase"
 
 
-import firebase from "firebase/app"
-import 'firebase/firestore'
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAAwYqwOBkIbZMsLp91G1dMk8aKM8EK7Og",
-    authDomain: "kouraya-company.firebaseapp.com",
-    projectId: "kouraya-company",
-    storageBucket: "kouraya-company.appspot.com",
-    messagingSenderId: "245245977401",
-    appId: "1:245245977401:web:f35ea1d846a0666faebee8",
-    measurementId: "G-NXZQ8G7ZZH"
-  };
-
-const firebase_app = firebase.initializeApp(firebaseConfig)
-const db = firebase_app.firestore()
-
-db.collection("villages").doc("bamako").get().then((doc) => {
+database.collection("villages").doc("bamako").get().then((doc) => {
   console.log(doc.data())
 });
 
--- frontend-kouraya/src/firebase.js
+++ frontend-kouraya/src/firebase.js
@@ -1,13 +1,18 @@
-// import firebase from "firebase"
+import firebase from "firebase/app"
+import "firebase/firestore"
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAAwYqwOBkIbZMsLp91G1dMk8aKM8EK7Og",
-//     authDomain: "kouraya-company.firebaseapp.com",
-//     projectId: "kouraya-company",
-//     storageBucket: "kouraya-company.appspot.com",
-//     messagingSenderId: "245245977401",
-//     appId: "1:245245977401:web:f35ea1d846a0666faebee8",
-//     measurementId: "G-NXZQ8G7ZZH"
-//   };
+const firebaseConfig = {
+    apiKey: "AIzaSyAAwYqwOBkIbZMsLp91G1dMk8aKM8EK7Og",
+    authDomain: "kouraya-company.firebaseapp.com",
+    projectId: "kouraya-company",
+    storageBucket: "kouraya-company.appspot.com",
+    messagingSenderId: "245245977401",
+    appId: "1:245245977401:web:f35ea1d846a0666faebee8",
+    measurementId: "G-NXZQ8G7ZZH"
+  };
 
-// export default firebase_app = firebase.initializeApp(firebaseConfig)
+// variables to export
+const firebase_app = firebase.initializeApp(firebaseConfig)
+const db = firebase_app.firestore()
+
+export const database = db
