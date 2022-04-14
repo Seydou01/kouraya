451feeafc4defde09ad18f9275cb28feb201763b
Memphis Malik <thisismemphismalik@gmail.com>
2022-04-14 02:12:48 +0000

    Firebase First Test

--- frontend-kouraya/src/App.js
+++ frontend-kouraya/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+// import { firebase_app } from './firebase';
+
+import firebase from "firebase/app"
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAAwYqwOBkIbZMsLp91G1dMk8aKM8EK7Og",
+    authDomain: "kouraya-company.firebaseapp.com",
+    projectId: "kouraya-company",
+    storageBucket: "kouraya-company.appspot.com",
+    messagingSenderId: "245245977401",
+    appId: "1:245245977401:web:f35ea1d846a0666faebee8",
+    measurementId: "G-NXZQ8G7ZZH"
+  };
+
+const firebase_app = firebase.initializeApp(firebaseConfig)
+const db = firebase_app.firestore()
+
+db.collection("villages").doc("bamako").get().then((doc) => {
+  console.log(doc.data())
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ frontend-kouraya/src/firebase.js
@@ -0,0 +1,13 @@
+// import firebase from "firebase"
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAAwYqwOBkIbZMsLp91G1dMk8aKM8EK7Og",
+//     authDomain: "kouraya-company.firebaseapp.com",
+//     projectId: "kouraya-company",
+//     storageBucket: "kouraya-company.appspot.com",
+//     messagingSenderId: "245245977401",
+//     appId: "1:245245977401:web:f35ea1d846a0666faebee8",
+//     measurementId: "G-NXZQ8G7ZZH"
+//   };
+
+// export default firebase_app = firebase.initializeApp(firebaseConfig)
